56f4ad88ee032ff4c3f137367341a71616c6ba36
Domenico Marchese <mdmengo@gmail.com>
2018-07-20 09:08:55 +0200

    Clean folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature: string = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 
 }
