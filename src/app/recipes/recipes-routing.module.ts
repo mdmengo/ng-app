384df0bc31583c2e32ea11f490a65793628009e4
Domenico Marchese <dmarchese@deloittedigital.com.mt>
2018-07-25 14:55:47 +0200

    Optimizations - Lazy Loading

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -8,7 +8,7 @@ import { RecipeDetailComponent } from '../recipes/recipe-detail/recipe-detail.co
 import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
 
 const recipesRoutes: Routes = [
-  { path: 'recipes', component: RecipesComponent, children: [
+  { path: '', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
     { path: ':id', component: RecipeDetailComponent },
